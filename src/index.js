2298486c109ae808c5a23477f3a59017fc7419ea
legndery <ar_itra@yahoo.com>
2018-10-19 02:38:20 +0530

    adding es5 support

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ if(argv.length === 1){
 const nKey = fs.readFileSync(path.resolve(__dirname , '../data/key')).toString();
 if(!nKey){
     console.log(`Usage: hotp-gen [key]\n\nOutputs RFC4662 OTP\nIf <Key> is present the secret Key will be set.`)
-    return;
+    process.exit(0);
 }
 const jsOTP = require('./modules/HOTP');
 var hotp = new jsOTP.hotp();
@@ -23,8 +23,8 @@ try{
     const time = Math.round(((new Date().getTime() / (1000.0))+ OFFSET)/COUNTER);
     const hmacCode = hotp.getOtp(nKey, time);
     const remainingTime =  COUNTER - Math.round(new Date().getTime() /1000.0)%60;
-    console.log(hmacCode);
-    !onlyOTP && console.log(`Remaining Time: ${remainingTime}secs`)
+    process.stdout.write(hmacCode);
+    !onlyOTP && console.log(`\nRemaining Time: ${remainingTime}secs`)
 }catch(e) {
     console.log("Invalid Key");
 }
