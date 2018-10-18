691fce6c9aa17a1888ef54c60de43a403eb835ac
legndery <ar_itra@yahoo.com>
2018-10-19 02:10:29 +0530

    #2: added -g script

--- src/index.js
+++ src/index.js
@@ -6,17 +6,20 @@ const OFFSET = -30;
 if(argv.length === 1){
     const nKey = argv[0];
     fs.writeFileSync('data/key', nKey.trim());
-}else{
-    const nKey = fs.readFileSync('data/key').toString();
-    const jsOTP = require('./modules/HOTP');
-    var hotp = new jsOTP.hotp();
-    try{
-        const time = Math.round(((new Date().getTime() / (1000.0))+ OFFSET)/COUNTER);
-        const hmacCode = hotp.getOtp(nKey, time);
-        const remainingTime =  COUNTER - Math.round(new Date().getTime() /1000.0)%60;
-        console.log(hmacCode);
-        console.log(`Remaining Time: ${remainingTime}secs`)
-    }catch(e) {
-        console.log("Invalid Key");
-    }
+}
+const nKey = fs.readFileSync('data/key').toString();
+if(!nKey){
+    console.log(`Usage: hotp-gen [key]\n\nOutputs RFC4662 OTP\nIf <Key> is present the secret Key will be set.`)
+    return;
+}
+const jsOTP = require('./modules/HOTP');
+var hotp = new jsOTP.hotp();
+try{
+    const time = Math.round(((new Date().getTime() / (1000.0))+ OFFSET)/COUNTER);
+    const hmacCode = hotp.getOtp(nKey, time);
+    const remainingTime =  COUNTER - Math.round(new Date().getTime() /1000.0)%60;
+    console.log(hmacCode);
+    console.log(`Remaining Time: ${remainingTime}secs`)
+}catch(e) {
+    console.log("Invalid Key");
 }
